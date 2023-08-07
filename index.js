146ac7c0c6d3be005fd5ec85f98039cf0956c10f
sohail20 <49647481+sohail20@users.noreply.github.com>
2023-08-07 19:18:08 +0500

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post('/webhook', (req, res) => {
 });
 
 app.get("/testing", (req, res) => {
-  res.write('A Monk in Cloud'); //write a response to the client
+  res.write('A Monk in Cloudzsdsd'); //write a response to the client
   res.end(); //end the response
 })
 
