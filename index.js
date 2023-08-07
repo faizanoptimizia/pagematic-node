448180ce15ce25e978c8e48d051862294b551efe
sohail20 <49647481+sohail20@users.noreply.github.com>
2023-08-07 19:16:49 +0500

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.post('/webhook', (req, res) => {
   const calculatedSignature = `sha256=${hmac.digest('hex')}`;
 
   if (crypto.timingSafeEqual(Buffer.from(signature), Buffer.from(calculatedSignature))) {
-    console.log("req.body", req.body)
+    console.log("req.bodyqwe", req.body)
     // Valid payload, do something with the new commit information
     console.log('New commit pushed:', req.body.head_commit.message);
   } else {
